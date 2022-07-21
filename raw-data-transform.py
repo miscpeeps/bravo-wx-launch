2c4b68e13a98cd6741614cd321340852bf7497f9
miscpeeps <michaelscottpeeples@gmail.com>
2022-07-20 20:42:39 -0400

    removed blank data, updated transform code, fixed spelling

--- raw-data-transform.py
+++ raw-data-transform.py
@@ -5,7 +5,12 @@ import os
 import datetime
 import csv
 import logging
+from typing import Dict
 import pandas as pd
+
+# import other python files for data transform
+import merlin_transform
+import amps_low_transform
 
 # start logging
 timestamp = str(datetime.datetime.now())
@@ -26,6 +31,8 @@ def make_results_directory(timestamp: str) -> str:
 
     new_dir = "./data-transform-" + timestamp +"/"
 
+    print("Attempting to create results directory:" + new_dir)
+
     # make directory
     if not os.path.exists(new_dir):
         try:
@@ -35,6 +42,8 @@ def make_results_directory(timestamp: str) -> str:
             logging.error("Can't create destination directory %s", new_dir)
             raise OSError("Can't create destination directory (%s)!" % (new_dir)) 
     
+    print("Successfully created results directory")
+
     return new_dir
 
 # create list of all folders in raw-data directory
@@ -42,9 +51,13 @@ def raw_data_directory_list(raw_data_directory: str) -> list:
     """Scans all subdirectories in raw-data directory.
     Returns list of strings of subdirectories in raw-data directory"""
 
+    print("Attempting to scan raw-data directory")
+
     dirs_list = [f.name for f in os.scandir(raw_data_directory) if f.is_dir()]
-    logging.debug("Found the following raw-data directories:")
+    logging.debug("Found %s raw-data directories:", str(len(dirs_list)))
     logging.debug(dirs_list)
+
+    print("Successfully scanned raw-data directory, found " + str(len(dirs_list)) + " directories")
 
     return dirs_list
 
@@ -55,28 +68,83 @@ def raw_data_files_dict(raw_data_folders: list) -> dict:
     key = string -> directory name
     value = list -> files in directory"""
     
+    number_files_scanned = 0
+
+    print("Attempting to scan for files in raw-data directory")
+
     raw_data_dict = {}
     for index, value in enumerate(raw_data_folders):
         # make path string for file scan location
         path = "./raw-data/" + raw_data_folders[index] + "/"
         logging.debug("Scanning folder %s", path)
         files_list = [f.name for f in os.scandir(path) if f.is_file()]
+        number_files_scanned += len(files_list)
         if len(files_list) < 7:
-            logging.warning("Found %s files:", str(len(files_list)))
+            logging.warning("Found %s files (7 files expected):", str(len(files_list)))
         else:
             logging.debug("Found %s files:", str(len(files_list)))
         logging.debug(files_list)
         raw_data_dict[path] = files_list
     
-    logging.debug("Returned raw data file dictionary")
+    logging.debug("Returned raw data file dictionary, found %s files", str(number_files_scanned))
     logging.debug(raw_data_dict)
+
+    print("Successfully scanned raw-data files, found " + str(len(raw_data_dict)) + " files")
 
     return raw_data_dict
 
-def test_transform(raw_data_files: dict, results_directory: str) -> None:
-    """Test transform that loads and works with csvs to generate a new csv"""
+def make_events_dict(events_list_file_path: str) -> dict:
+    """Extracts event (launches and scrubs) times and dates from a
+    given input file. Returns a dictionary with a key of dates and
+    value pair of datetime objects corresponding to event times"""
 
-    # number of unique data points processed
+    print("Attempting to create list of launch/scrub dates and times")
+
+    events_list = {}
+
+    # load and transform launch events
+    try:
+        df = pd.read_csv(events_list_file_path)
+        logging.debug("Loaded events list file %s", events_list_file_path)
+    except:
+        logging.error("Can't find events file to load %s", events_list_file_path)
+        raise OSError("Can't find events file to load (%s)!" % (events_list_file_path))
+    logging.debug("Full events table:")
+    logging.debug(df)
+    
+    # isolate data
+    df.drop(df.columns.difference(["time (z)","launch date"]), axis=1, inplace=True)
+    logging.debug("Edited events table:")
+    logging.debug(df)
+
+    # translate dataframe info to dictionary key (date):value (time)
+    for index, row in df.iterrows():
+        events_list[row["launch date"]] = row["time (z)"]
+    
+    logging.debug("%s event times by date string:", str(len(events_list)))
+    logging.debug(events_list)    
+
+    # transform times into associated datetime objects
+    for key in events_list:
+        # split launch csv MM/DD/YYYY date strings into parts
+        date_parts = key.split("/")
+        time_parts = events_list[key].split(":")
+        # replace value with datetime object
+        events_list[key] = datetime.datetime(int(date_parts[2]), int(date_parts[0]), int(date_parts[1]),
+                                             int(time_parts[0]), int(time_parts[1]))
+    
+    logging.debug("%s event datetime objects by date string:", str(len(events_list)))
+    logging.debug(events_list)    
+
+    print("Successfully created launch/scrub list for " + str(len(events_list)) + " events")
+    
+    return events_list
+
+def transform_data(raw_data_files: dict, results_directory: str, event_times: dict) -> None:
+    """Transforms raw data to format suitable for ML modeling. Writes consolidated
+    data files to disk as new csv files"""
+
+    # gather neat info
     total_data_points = 0
 
     # dict to hold transformed dataframes
@@ -141,7 +209,6 @@ def test_transform(raw_data_files: dict, results_directory: str) -> None:
                 logging.warning("%s is not a valid data file. Ignoring", file_name)
     logging.debug("Loaded %s total data points", "{:,}".format(total_data_points))
 
- 
 # main program
 results_directory = make_results_directory(timestamp)
 
@@ -149,4 +216,8 @@ raw_data_folders = raw_data_directory_list("./raw-data")
 
 raw_data_files = raw_data_files_dict(raw_data_folders)
 
-test_transform(raw_data_files, results_directory)
+event_times = make_events_dict("launches.csv")
+
+transform_data(raw_data_files, results_directory, event_times)
+
+# pass datetime object and file path string of launch/scrub zulu to transform
---
+++ wind_profiler_915_transform.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+from datetime import timedelta, datetime
+
+
+def time_date(row):
+    row['datetime'] = f"{row['Event Date']} {row['Event Time']}"
+    row['datetime'] = pd.to_datetime(row['datetime'])
+    return row
+
+
+def direction_sep(row):
+    row['Direction'] = np.radians(row['Direction'])
+    row['Direction_x'] = np.sin(row['Direction'])
+    row['Direction_y'] = np.cos(row['Direction'])
+    return row
+
+
+def wind_profiler_915(path, launchtime):
+    # create list of 5 minute increments starting at 4 hours before launch
+    time_init = launchtime - timedelta(hours=4)
+    time_list = [time_init]
+    while time_list[-1] < launchtime:
+        time_list.append(time_list[-1] + timedelta(minutes=5))
+    # load in the relevant csv
+    init_df = pd.read_csv(path)
+
+    # separate out the wind directions based on the angle provided
+    init_df = init_df.apply(direction_sep, axis=1)
+
+    # copy out the event date, time, profiler, height, speed, direction in x direction
+    wp_915_df = init_df.loc[:,
+                ['Event Date', 'Event Time', 'Profiler', 'Height', 'Speed', 'Direction_x', 'Direction_y']]
+    # adjust dates to create a datetime that matches the actual launch times
+    wp_915_df = wp_915_df.apply(time_date, axis=1)
+    offset = (time_init - wp_915_df.iloc[0, -1])
+    wp_915_df['datetime'] = wp_915_df['datetime'] + offset
+    # get rid of irrelevant info
+    wp_915_df.drop(['Event Date', 'Event Time'], axis=1, inplace=True)
+
+    # sample the unique profilers
+    profilers = wp_915_df['Profiler'].unique()
+    # create the output dataframe from the time_list
+    output_df = pd.DataFrame(index=time_list)
+    dummy_data = np.ones(49) * 9999
+
+    ##iterate through each profiler
+    for profiler in profilers:
+
+        # latest_speed=pd.DataFrame(index=time_list,data=dummy_data)
+        # latest_dir=pd.DataFrame(index=time_list,data=dummy_data)
+        # create dataframe for each profiler, organized by datetime
+        profiler_df = wp_915_df.loc[wp_915_df['Profiler'] == profiler].copy()
+        profiler_df.sort_values(by=['Height', 'datetime'], inplace=True)
+        profiler_df.reset_index(drop=True, inplace=True)
+
+        # create binning by height
+        height_max = [0, 0.8, 1.5, 10]
+
+        # iterate through each bin
+        for ind in range(len(height_max) - 1):
+            bin_name = f'Max Height: {height_max[ind + 1]} '
+            # filter out the relevant bin from the profiler dataframe
+            bin_df = profiler_df[
+                (profiler_df['Height'] < height_max[ind + 1]) & (profiler_df['Height'] > height_max[ind])].copy()
+
+            # find the max speed for each datetime in the bin
+            speed_df = bin_df.groupby(by='datetime').max()['Speed']
+            # backfill data, then forward fill the rest
+            speed_df.fillna(method='bfill', inplace=True)
+            speed_df.fillna(method='ffill', inplace=True)
+
+            if speed_df.count() > 0:
+                latest_speed = speed_df
+            else:
+                speed_df = latest_speed
+
+            # find the variance for the x and y components of wind direction
+            dir_df = bin_df.groupby(by='datetime').var()[['Direction_x', 'Direction_y']]
+            # backfill data, then forward fill the rest
+            dir_df.fillna(method='bfill', inplace=True)
+            dir_df.fillna(method='ffill', inplace=True)
+            # sum the variances
+            dir_df['Direction Variance'] = dir_df['Direction_x'] + dir_df['Direction_y']
+            dir_df.drop(['Direction_x', 'Direction_y'], axis=1, inplace=True)
+
+            if dir_df['Direction Variance'].count() > 0:
+                latest_dir = dir_df
+            else:
+                dir_df = latest_dir
+
+            # combine final solutions into one df, rename to relevant column names
+            df = pd.concat([speed_df, dir_df], axis=1)
+            df.rename(columns={"Speed": f'{profiler} {bin_name} km Speed (m/s)',
+                               "Direction Variance": f'{profiler} {bin_name} km Direction (var)'}, inplace=True)
+            output_df = output_df.merge(df, left_index=True, right_index=True)
+
+    # interpolate between missing times
+    missing_times = list(set(time_list) - set(output_df.index.to_list()))
+    data_interp = pd.DataFrame(index=missing_times, columns=output_df.columns)
+    interp_df = pd.concat([output_df, data_interp])
+    interp_df.sort_index(inplace=True)
+    interp_df.interpolate(inplace=True)
+
+    return interp_df
